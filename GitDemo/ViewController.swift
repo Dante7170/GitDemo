1e28075fabab64b5a11a22fef850f83fc3b172d9
Komilov Jasur <komilovjasur@yandex.ru>
2020-07-21 16:36:24 +0300

    Initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Jasurbek Komilov on 21.07.2020.
+//  Copyright © 2020 Jasurbek Komilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
